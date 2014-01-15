7c7a562da4195f125e3acba952941be93aa445d8
LLFRealms <support@llfrealms.com>
2014-01-14 23:59:11 -0500

    everything for town creation working but setting the format

--- src/com/llfrealms/THChatLink/THCLink.java
+++ src/com/llfrealms/THChatLink/THCLink.java
@@ -16,6 +16,8 @@ public final class THCLink extends JavaPlugin
 	public ConsoleCommandSender consoleMessage = Bukkit.getConsoleSender();
 	public String pluginname = "TownyHerochatLink";
 	public ArrayList<String> perms = new ArrayList<String>(); //list of permissions from the config
+	public boolean createN; //boolean for whether or not to create new channels for nations
+	public boolean createT; //boolean for whether or not to create new channels for towns
 	
 	@Override
 	public void onEnable()
@@ -23,6 +25,7 @@ public final class THCLink extends JavaPlugin
 		this.saveDefaultConfig();
     	this.getConfig();
 		new THCListeners(this);
+		THCSetup();
 		Utilities.sendMessage(consoleMessage, "&6" + pluginname + " enabled!");
     }
 
@@ -38,6 +41,24 @@ public final class THCLink extends JavaPlugin
     	for(String s : permissions)
     	{
     		perms.add(s);
+    	}
+    	if(getConfig().isBoolean("createFor.nations"))
+    	{
+    	createN = getConfig().getBoolean("createFor.nations");
+    	Utilities.sendMessage(consoleMessage, "&6Nations entry is " + createN);
+    	}
+    	else
+    	{
+    		Utilities.sendMessage(consoleMessage, "&6Nations entry is not a boolean value");
+    	}
+    	if(getConfig().isBoolean("createFor.towns"))
+    	{
+    		createT = getConfig().getBoolean("createFor.towns");
+    		Utilities.sendMessage(consoleMessage, "&6Towns entry is " + createT);
+    	}
+    	else
+    	{
+    		Utilities.sendMessage(consoleMessage, "&6Towns entry is not a boolean value");
     	}
     }
     
--- src/com/llfrealms/THChatLink/util/THCListeners.java
+++ src/com/llfrealms/THChatLink/util/THCListeners.java
@@ -8,6 +8,7 @@ import org.bukkit.event.Listener;
 
 import com.llfrealms.THChatLink.THCLink;
 import com.palmergames.bukkit.towny.event.NewTownEvent;
+import com.palmergames.bukkit.towny.object.Town;
 
 public class THCListeners implements Listener {
 	
@@ -19,32 +20,51 @@ public class THCListeners implements Listener {
 	String create = "ch create channel nick"; //string holding the command for creating a new channel
 	String delete = "ch remove channel"; //string holding the command for deleting a channel
 	String color = "ch set channel color words"; //string holding the command for changing the color of the channel where "words" is the color from the config
-	String format = "ch set format words"; //command for changing the format of the channel
-	String addGroup = "perm player addgroup g:words"; // command to add a group for the town/nation to the player words is the town/nation name
+	String format = "ch set channel format words"; //command for changing the format of the channel
+	String addGroup = "perm player words addgroup g:town"; // command to add a group for the town/nation to the player words is the town/nation name
 	String parentGroup = "perm groups g:town setparents g:nation";
 	String createGroup = "perm group g:words create"; // command to create a group for the town/nation where words is the town/nation name
-	String addPerms = "perm group g:words set permission"; //command to set a permission for the town/nation group to true where words is the town/nation name
+	String addPerms = "perm group g:words set "; //command to set a permission for the town/nation group to true where words is the town/nation name
 	
-	boolean createN = plugin.getConfig().getBoolean("createFor.nations"); //boolean for whether or not to create new channels for nations
-	boolean createT = plugin.getConfig().getBoolean("createFor.towns"); //boolean for whether or not to create new channels for towns
 	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onTownCreation(NewTownEvent event) 
 	{
-		String town = event.getTown().toString();
+		Town createdTown = event.getTown();
+		String town = createdTown.toString();
 		town.replaceAll("_", "");
+		
 		String nick = town.substring(0, 4);
+		
 		String command = create.replace("channel", town); //replace "channel" with the town name
-		command.replace("nick", nick); //replace "nick" with the first four letters of the town name
+		command = command.replace("nick", nick); //replace "nick" with the first four letters of the town name
 		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command); //create the channel
 		
 		command = color.replace("words", plugin.getConfig().getString("towns.color")); //change the command to changing the color and change "words" to the color in the config
+		command = command.replace("channel", town);
 		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command); //change the color to the color from the config
 		
 		command = format.replace("words", plugin.getConfig().getString("towns.format")); //change the command to setting the format and change "words" to the format in the config
+		command = command.replace("channel", town);
 		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command); //change the format to the one defined in the config
+		Utilities.sendMessage(plugin.consoleMessage, "&f" + command);
 		
 		command = createGroup.replace("words", town); //change the command to creating a group for the town
+		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command); //make a group for the town
+		
+		String mayor = createdTown.getMayor().toString(); //string to hold the creators name
+		command = addGroup.replace("town", town); //set command to adding mayor to the towns group
+		command = command.replace("words", mayor);
+		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command); //add mayor to town group
+		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "sudo " + mayor + " ch join " + town); //add mayor to town chat
+		
+		command = addPerms.replace("words", town);
+		String command2;
+		for(int i = 0; i < plugin.perms.size(); i++)
+		{
+			command2 = command + plugin.perms.get(i) + "." + town; //add the channel name to the end of each permission to be added
+			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command2); //add perms to town group
+		}
 	}
 	
 	
